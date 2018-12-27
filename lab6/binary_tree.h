d1d68c4b7f70d9fede4f2f236318992401e26405
anastasiya11 <43649475+anastasiya11@users.noreply.github.com>
2018-12-28 01:04:56 +0300

    Create binary_tree.h

---
+++ lab6/binary_tree.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "array.h"
+
+
+class TBinaryTree
+{
+public:
+	TBinaryTree();
+	void push(void *value);
+	int Size();
+	~TBinaryTree();
+//private:
+	TBinaryTree *left;
+	TBinaryTree *right;
+	//T data;
+	void *data;
+	int size;
+};
