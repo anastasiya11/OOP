dc40fa4d9506472e32a2dd6cdb423b2a7a11a4e9
anastasiya11 <43649475+anastasiya11@users.noreply.github.com>
2018-12-30 23:10:56 +0300

    Update lab9.cpp

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -1,60 +1,59 @@
-#include "stdafx.h"
 #include <future>
 #include <functional>
 #include <random>
 #include <thread>
 
-#include "Tree.h"
-#include "Vector.h"
-#include "Figure.h"
-#include "Rectangle.h"
+#include "binary_tree.h"
+#include "array.h"
+#include "figure.h"
+#include "rectangle.h"
 
 int main()
 {
-	Tree<Rectangle> treeRect;
+	TArray <Rectangle> arrRect;
 	typedef std::function<void(void) > command;
-	Vector<command> vecCmd;
+	TBinaryTree <command> btrCmd;
 
 	
 
 	command cmdInsert = [&]() {
-		std::cout << "Creating random tree" << std::endl;
+		std::cout << "Creating random array" << std::endl;
 		std::default_random_engine generator(time(NULL));
 		std::uniform_int_distribution<int> distribution(1, 20);
 		for (int i = 0; i < 10; i++) {
 			int side_a = distribution(generator);
 			int side_b = distribution(generator);
 			std::shared_ptr<Rectangle> ptr = std::make_shared<Rectangle>(side_a, side_b);
-			int key = distribution(generator);
-			treeRect.insert(ptr, 0, key);
+			int ind = distribution(generator);
+			arrRect.ElementInsert(ptr, ind);
 		}
 	};
 
 	command cmdPrint = [&]() {
-		std::cout << "Printing tree" << std::endl;
-		treeRect.print();
+		std::cout << "Printing array" << std::endl;
+		arrRect.print();
 	};
 
 	command removeIfEqualN = [&]() {
-		if (treeRect.empty()) {
-			std::cout << "Tree is empty" << std::endl;
+		if (arrRect.empty()) {
+			std::cout << "Array is empty" << std::endl;
 
 		}
 		else {
 			size_t n;
 			std::cout << "Enter N: ";
 			std::cin >> n;
-			treeRect.remove(n);
+			arrRect.ElementDelete(n);
 		}
 	};
 
-	vecCmd.pushBack(std::shared_ptr<command>(&cmdInsert, [](command*){}));
-	vecCmd.pushBack(std::shared_ptr<command>(&cmdPrint, [](command*) {}));
-	vecCmd.pushBack(std::shared_ptr<command>(&removeIfEqualN, [](command*) {}));
-	vecCmd.pushBack(std::shared_ptr<command>(&cmdPrint, [](command*) {}));
+	btrCmd.push(std::shared_ptr<command>(&cmdInsert, [](command*){}));
+	btrCmd.push(std::shared_ptr<command>(&cmdPrint, [](command*) {}));
+	btrCmd.push(std::shared_ptr<command>(&removeIfEqualN, [](command*) {}));
+	btrCmd.push(std::shared_ptr<command>(&cmdPrint, [](command*) {}));
 
-	while (!vecCmd.empty()) {
-		std::shared_ptr<command> cmd = vecCmd.remove(0);
+	while (!btrCmd.empty()) {
+		std::shared_ptr<command> cmd = btrCmd.remove(0);
 		std::future<void> ft = std::async(*cmd);
 		ft.get();
 
