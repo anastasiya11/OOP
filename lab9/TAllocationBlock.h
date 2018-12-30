f9e769c3c3c445199a729747ff4359ed76832e7e
anastasiya11 <43649475+anastasiya11@users.noreply.github.com>
2018-12-30 22:33:35 +0300

    Update TAllocationBlock.h

--- lab9/TAllocationBlock.h
+++ lab9/TAllocationBlock.h
@@ -1,23 +1,20 @@
-#pragma once
-#include <cstdlib>
-#include <iostream>
+#ifndef TALLOCATIONBLOCK_H
+#define TALLOCATIONBLOCK_H
 
-class TAllocationBlock
-{
+
+#include <cstdlib>
+class TAllocationBlock {
 public:
 	TAllocationBlock(size_t size, size_t count);
 	void *allocate();
 	void deallocate(void *pointer);
-	bool hasFreeBlocks();
-
-	~TAllocationBlock();
-
+	bool has_free_blocks();
+	virtual ~TAllocationBlock();
 private:
-	size_t size;
-	size_t count;
-
-	char * usedBlocks;
-	void ** freeBlocks;
-
-	size_t freeCount;
+	size_t _size;
+	size_t _count;
+	char *_used_blocks;
+	void **_free_blocks;
+	size_t _free_count;
 };
+#endif /* TALLOCATIONBLOCK_H */
